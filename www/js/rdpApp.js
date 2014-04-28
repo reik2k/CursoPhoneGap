ce1320b712fa68fa1577b042a389985c0fe7db70
abel2k <reik2k@hotmail.com>
2014-04-28 23:15:58 +0200

    Crear función comprobar conexión de RED después de que phonegap esté cargado.

--- www/js/rdpApp.js
+++ www/js/rdpApp.js
@@ -3,6 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/** Called when browser load this page*/
+function init(){
+   document.addEventListener("deviceready", onDeviceReady, false);
+}
+
 function onDeviceReady(){
     document.getElementById("deviceName").innerHTML
         = device.name; 
@@ -13,10 +18,21 @@ function onDeviceReady(){
     document.getElementById("platformVersion").innerHTML
         = device.version; 
     document.getElementById("uuid").innerHTML
-        = device.uuid;   
+        = device.uuid;
+    //checkConnection();
 }
-/** Called when browser load this page*/
-function init(){
-   document.addEventListener("deviceready", onDeviceReady, false);
+function checkConnection() 
+{
+    var networkState = navigator.network.connection.type;
 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Conexión desconocida';
+    states[Connection.ETHERNET] = 'Conexión ethernet';
+    states[Connection.WIFI]     = 'Conexión WiFi';
+    states[Connection.CELL_2G]  = 'Conexión movil 2G';
+    states[Connection.CELL_3G]  = 'Conexión movil 3G';
+    states[Connection.CELL_4G]  = 'Conexión movil 4G';
+    states[Connection.NONE]     = 'Sin conexión';
+
+    alert('Tipo de conexión: ' + states[networkState]);
 }
