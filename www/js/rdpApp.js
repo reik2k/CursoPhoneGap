8527cf13d313b0c930ad400e39465bd1dee47c84
abel2k <reik2k@hotmail.com>
2014-05-14 07:19:59 +0200

    modificación desde la función init()

--- www/js/rdpApp.js
+++ www/js/rdpApp.js
@@ -4,79 +4,32 @@
  * and open the template in the editor.
  */
 /** Called when browser load this page*/
+
 function init()
 {
-   document.addEventListener("deviceready", onDeviceReady, false);
+    var table = document.getElementById('example1');
+    document.addEventListener("deviceready",onDeviceReady, false);
+    
+    table.style.display = 'block';
 }
-
 function onDeviceReady()
 {
-
-    
-    
+   
 }
-
-function checkConnection() 
-{
-    var networkState = navigator.network.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Conexión desconocida';
-    states[Connection.ETHERNET] = 'Conexión ethernet';
-    states[Connection.WIFI]     = 'Conexión WiFi';
-    states[Connection.CELL_2G]  = 'Conexión movil 2G';
-    states[Connection.CELL_3G]  = 'Conexión movil 3G';
-    states[Connection.CELL_4G]  = 'Conexión movil 4G';
-    states[Connection.NONE]     = 'Sin conexión';
-
-    alert('Tipo de conexión: ' + states[networkState]);
-}
-
-function callSettings()
-{
-    var div = document.getElementById("example1");
-    
-    div.style.display= 'block';
-    
-    document.getElementById("deviceName").innerHTML
-        = device.name; 
-    document.getElementById("version").innerHTML
-        = device.phonegap; 
-    document.getElementById("mobilePlatform").innerHTML
-        = device.platform; 
-    document.getElementById("platformVersion").innerHTML
-        = device.version; 
-    document.getElementById("uuid").innerHTML
-        = device.uuid;
-}
-
-function callAlert()
-{
-  
-}
-function callback()
-{
-    if (true)
-    {
-        alert('notification OK!');
-    }
-}
-
 function navigator(destination)
 {
     switch(destination)
     {
     case 1:  
     {
-       var ref = window.open(   encodeURI('http://www.google.es'),
-                                '_blank',
-                                'location=yes');
        // close InAppBrowser after 5 seconds
        setTimeout(
             function() 
             {
+                var ref = window.open(      encodeURI('http://www.google.es'),
+                                            '_blank',
+                                            'location=yes');
                 ref.close();
-                alert('5s');
             }, 5000);
          
          break;
@@ -91,16 +44,27 @@ function navigator(destination)
     case 3:
     {
         
-        navigator.notification.alert(
-                'mensage',
-                callback,
-                'title',
-                'botonOK'
-                );
+        
         break;
     }
     default:
         alert('default');
     }
     
+}
+function callAlert()
+{
+  navigator.notification.alert(
+                'mensage',
+                callback,
+                'title',
+                'botonOK'
+                );
+}
+function callback()
+{
+    if (true)
+    {
+        alert('notification OK!');
+    }
 }
