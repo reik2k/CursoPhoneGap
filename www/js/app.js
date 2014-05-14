af23d59886775955fd40c53fc1ce5345477fe35b
abel2k <reik2k@hotmail.com>
2014-05-14 23:52:34 +0200

    STEP 5: Push notification overrride... nofunciona pero se guardan los cambios.

--- www/js/app.js
+++ www/js/app.js
@@ -28,3 +28,25 @@
     }
 
 }());
+
+function init()
+{
+            alert("Some help here...")
+
+    documet.addEventListener('deviceready',
+    function()
+    {
+        if (navigator.notification) 
+        {   // Override default HTML alert with native dialog
+            window.alert = function (message) 
+            {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    });
+}
